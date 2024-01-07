18a3add0cd6d385f81060fc1634d601d33755ebc
vquijano12 <vquijano7224@gmail.com>
2024-01-07 12:11:12 -0500

    Added Window

--- ClickAdventure/src/Main/GameManager.java
+++ ClickAdventure/src/Main/GameManager.java
@@ -2,9 +2,15 @@ package Main;
 
 public class GameManager {
 
-	public static void main(String[] args) {
-		
+	UI ui = new UI(this);
 
+	public static void main(String[] args) {
+
+		new GameManager();
+
+	}
+	public GameManager() {
+		
 	}
 
 }
--- ClickAdventure/src/Main/UI.java
+++ ClickAdventure/src/Main/UI.java
@@ -1,5 +1,32 @@
 package Main;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 public class UI {
+
+	GameManager gm;
+
+	JFrame window;
+
+	public UI(GameManager gm) {
+
+		this.gm = gm;
+
+		createMainField();
+
+		window.setVisible(true);
+	}
+
+	public void createMainField() {
+
+		window = new JFrame();
+		window.setSize(800,600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setBackground(Color.black);
+		window.setLayout(null);
+
+	}
 
 }
