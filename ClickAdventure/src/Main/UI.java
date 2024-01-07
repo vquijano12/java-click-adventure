a94d1bc25c90ca9890fb9adaba68a5595031a8d8
vquijano12 <vquijano7224@gmail.com>
2024-01-07 13:12:36 -0500

    Added createBackground method

--- ClickAdventure/src/Main/UI.java
+++ ClickAdventure/src/Main/UI.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class UI {
@@ -11,14 +13,16 @@ public class UI {
 	GameManager gm;
 
 	JFrame window;
-
 	public JTextArea messageText;
+	public JPanel bgPanel[] = new JPanel[10];
+	public JLabel bgLabel[] = new JLabel[10];
 
 	public UI(GameManager gm) {
 
 		this.gm = gm;
 
 		createMainField();
+		createBackground();
 
 		window.setVisible(true);
 	}
@@ -40,6 +44,15 @@ public class UI {
 		messageText.setWrapStyleWord(true);
 		messageText.setFont(new Font("Book Antiqua", Font.PLAIN, 26));
 		window.add(messageText);
+	}
+
+	public void createBackground() {
+
+		bgPanel[1] = new JPanel();
+		bgPanel[1].setBounds(50,50,700,350);
+		bgPanel[1].setBackground(Color.blue);
+		bgPanel[1].setLayout(null);
+		window.add(bgPanel[1]);
 	}
 
 }
